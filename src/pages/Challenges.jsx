5c02a1a94554bcf8a7d505cd4866008548242caf
charlesmumo <charles.mumo@student.moringaschool.com>
2024-07-10 09:59:36 +0300

    landing page

---
+++ src/pages/Challenges.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FitnessChallengesPage = () => {
+  return (
+    <div>
+      <h2>Fitness Challenges</h2>
+      <p>Explore and participate in fitness challenges.</p>
+    </div>
+  );
+};
+
+export default FitnessChallengesPage;
---
+++ src/pages/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';  
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home-content">
+        <h1 id='welcome'>Welcome to FitMind</h1>
+        <p>FitMind helps you track your fitness, build habits, and enhance wellness through personalized challenges and mindfulness sessions.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
